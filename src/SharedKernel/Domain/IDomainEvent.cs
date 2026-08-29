6978be1d639710e59924ebdaea7d888c20a990e2
luaay <84683724+username@users.noreply.github.com>
2026-08-30 01:19:45 +0300

    feat: complete Episode 08 implementation

--- src/SharedKernel/Domain/IDomainEvent.cs
+++ src/SharedKernel/Domain/IDomainEvent.cs
@@ -2,6 +2,9 @@ using MediatR;
 
 namespace BuildFlow.SharedKernel.Domain;
 
+// عقد لكل شيء مهم حدث في النطاق، بصيغة الماضي
 public interface IDomainEvent : INotification
 {
+    Guid EventId { get; }            // لدعم منع التكرار
+    DateTime OccurredOnUtc { get; }  // توقيت الحدوث بالـ UTC
 }
